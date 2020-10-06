f82846e66292eacabbd82480adb948f964dc2aff
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-06 09:00:37 -0300

    :tada: initial project

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clean Architecture TDD',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
