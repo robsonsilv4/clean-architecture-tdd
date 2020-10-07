5d4483bc8144794fec6ede8a68872db165e4082f
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-07 14:36:44 -0300

    feat: add get random number trivia

---
+++ lib/features/number_trivia/domain/use_cases/get_random_number_trivia.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../../core/errors/failures.dart';
+import '../../../../core/use_cases/use_case.dart';
+import '../entities/number_trivia.dart';
+import '../repositories/number_trivia_repository.dart';
+
+class GetRandomNumberTrivia implements UseCase<NumberTrivia, NoParams> {
+  final NumberTriviaRepository repository;
+
+  GetRandomNumberTrivia(this.repository);
+
+  @override
+  Future<Either<Failure, NumberTrivia>> call(NoParams params) async {
+    return await repository.getRandomNumberTrivia();
+  }
+}
