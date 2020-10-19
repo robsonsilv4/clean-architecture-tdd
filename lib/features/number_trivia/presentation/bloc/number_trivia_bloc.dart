5dd01a9226ef5dd63fdd93279b485f04b79c86f3
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-19 16:52:33 -0300

    :white_check_mark: add number trivia bloc tests

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,15 +1,48 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
 
+import '../../../../core/utils/input_converter.dart';
+import '../../domain/use_cases/get_concrete_number_trivia.dart';
+import '../../domain/use_cases/get_random_number_trivia.dart';
 import 'bloc.dart';
 
+const String SERVER_FAILURE_MESSAGE = 'Server failure.';
+const String CACHE_FAILURE_MESSAGE = 'Cache failure.';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid input - The number must be a positive integer or zero.';
+
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    @required this.getConcreteNumberTrivia,
+    @required this.getRandomNumberTrivia,
+    @required this.inputConverter,
+  })  : assert(getConcreteNumberTrivia != null),
+        assert(getRandomNumberTrivia != null),
+        assert(inputConverter != null);
+
   @override
   NumberTriviaState get initialState => Empty();
 
   @override
   Stream<NumberTriviaState> mapEventToState(
     NumberTriviaEvent event,
-  ) async* {}
+  ) async* {
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEiter = inputConverter.stringToUnsignedInteger(
+        event.numberString,
+      );
+      yield* inputEiter.fold(
+        (failure) async* {
+          yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+        (integer) => throw UnimplementedError(),
+      );
+    }
+  }
 }
