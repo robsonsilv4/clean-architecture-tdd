c42bf5f6bc4cb0a7fb263dfbd4ba3c6fd87a0497
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-19 14:56:48 -0300

    :sparkles: add number trivia bloc

---
+++ lib/features/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+import '../../domain/entities/number_trivia.dart';
+
+@immutable
+abstract class NumberTriviaState extends Equatable {
+  NumberTriviaState([List props = const <dynamic>[]]) : super(props);
+}
+
+class Empty extends NumberTriviaState {}
+
+class Loading extends NumberTriviaState {}
+
+class Loaded extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  Loaded({@required this.trivia}) : super([trivia]);
+}
+
+class Error extends NumberTriviaState {
+  final String message;
+
+  Error({@required this.message}) : super([message]);
+}
