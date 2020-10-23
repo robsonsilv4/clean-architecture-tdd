4332d59a620b258f40c233e794bd0b9deadbdbac
robsonsilv4 <robsonsilv410@gmail.com>
2020-10-23 16:56:36 -0300

    :sparkles: finish number trivia bloc

--- lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/bloc/number_trivia_bloc.dart
@@ -1,9 +1,13 @@
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
 import 'package:meta/meta.dart';
 
+import '../../../../core/errors/failures.dart';
+import '../../../../core/use_cases/use_case.dart';
 import '../../../../core/utils/input_converter.dart';
+import '../../domain/entities/number_trivia.dart';
 import '../../domain/use_cases/get_concrete_number_trivia.dart';
 import '../../domain/use_cases/get_random_number_trivia.dart';
 import 'bloc.dart';
@@ -41,8 +45,42 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
         (failure) async* {
           yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
         },
-        (integer) => throw UnimplementedError(),
+        (integer) async* {
+          yield Loading();
+          final failureOrTrivia = await getConcreteNumberTrivia(
+            Params(number: integer),
+          );
+          yield* _eitherLoadedOrErrorState(failureOrTrivia);
+        },
       );
+    } else if (event is GetTriviaForRandomNumber) {
+      yield Loading();
+      final failureOrTrivia = await getRandomNumberTrivia(
+        NoParams(),
+      );
+      yield* _eitherLoadedOrErrorState(failureOrTrivia);
+    }
+  }
+
+  Stream<NumberTriviaState> _eitherLoadedOrErrorState(
+    Either<Failure, NumberTrivia> failureOrTrivia,
+  ) async* {
+    yield failureOrTrivia.fold(
+      (failure) => Error(
+        message: _mapFailureToMessage(failure),
+      ),
+      (trivia) => Loaded(trivia: trivia),
+    );
+  }
+
+  String _mapFailureToMessage(Failure failure) {
+    switch (failure.runtimeType) {
+      case ServerFailure:
+        return SERVER_FAILURE_MESSAGE;
+      case CacheFailure:
+        return CACHE_FAILURE_MESSAGE;
+      default:
+        return 'Unexpected error.';
     }
   }
 }
