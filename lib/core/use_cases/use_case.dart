43ef4e01952473c858c1053c5534948380a59831
robsonsilv4 <robsonsilv410@gmail.com>
2026-08-12 08:48:00 -0300

    fix: restore UseCase call contract

--- lib/core/use_cases/use_case.dart
+++ lib/core/use_cases/use_case.dart
@@ -1,8 +1,13 @@
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+
+import '../errors/failures.dart';
 
 class NoParams extends Equatable {
   @override
   List<Object> get props => [];
 }
 
-abstract class UseCase<Type, Params> {}
+abstract class UseCase<Type, Params> {
+  Future<Either<Failure, Type>> call(Params params);
+}
