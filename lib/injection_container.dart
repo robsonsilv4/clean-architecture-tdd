75c39258ecaec97a0c2a33887236bdb17a07724c
robsonsilv4 <robsonsilv410@gmail.com>
2026-08-12 08:48:18 -0300

    refactor: update dependency injection for new packages

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,6 +1,6 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
 import 'package:http/http.dart' as http;
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'core/network/network_info.dart';
@@ -55,12 +55,11 @@ Future<void> init() async {
   //! Core
   sl.registerLazySingleton(() => InputConverter());
   sl.registerLazySingleton<NetworkInfo>(
-    () => NetworkInfoImpl(sl()),
+    () => NetworkInfoImpl(InternetConnection()),
   );
 
   //! External
   final sharedPreferences = await SharedPreferences.getInstance();
   sl.registerLazySingleton(() => sharedPreferences);
   sl.registerLazySingleton(() => http.Client());
-  sl.registerLazySingleton(() => DataConnectionChecker());
 }
