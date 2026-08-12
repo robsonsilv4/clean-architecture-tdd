9f3afd0260b8004ccc14bc9f241a1f325d8ba52b
robsonsilv4 <robsonsilv410@gmail.com>
2026-08-12 08:48:04 -0300

    refactor: migrate core network to null safety

--- lib/core/network/network_info.dart
+++ lib/core/network/network_info.dart
@@ -1,14 +1,14 @@
-import 'package:data_connection_checker/data_connection_checker.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 abstract class NetworkInfo {
   Future<bool> get isConnected;
 }
 
 class NetworkInfoImpl implements NetworkInfo {
-  final DataConnectionChecker connectionChecker;
+  final InternetConnection connectionChecker;
 
   NetworkInfoImpl(this.connectionChecker);
 
   @override
-  Future<bool> get isConnected => connectionChecker.hasConnection;
+  Future<bool> get isConnected => connectionChecker.hasInternetAccess;
 }
